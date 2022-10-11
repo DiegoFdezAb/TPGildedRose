37ef8463ae046892b8993476c2155ee2e0580c62
Why-Not-There <Diego.FERNANDEZ_ABASCAL@etu.uca.fr>
2022-10-11 15:39:25 +0200

    Simplify calculations

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -14,40 +14,40 @@ class GildedRose {
             }
             if (items[i].name.equals("Aged Brie")) {
                 if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                    items[i].quality++;
                 }
-                items[i].sellIn = items[i].sellIn - 1;
+                items[i].sellIn--;
                 if (items[i].sellIn < 0) {
                     if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                        items[i].quality++;
                     }
                 }
             } else if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                    items[i].quality++;
                     if (items[i].sellIn < 11) {
                         if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
+                            items[i].quality++;
                         }
                     }
                     if (items[i].sellIn < 6) {
                         if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
+                            items[i].quality++;
                         }
                     }
                 }
-                items[i].sellIn = items[i].sellIn - 1;
+                items[i].sellIn--;
                 if (items[i].sellIn < 0) {
-                    items[i].quality = items[i].quality - items[i].quality;
+                    items[i].quality = 0;
                 }
             } else {
                 if (items[i].quality > 0) {
-                    items[i].quality = items[i].quality - 1;
+                    items[i].quality--;
                 }
-                items[i].sellIn = items[i].sellIn - 1;
+                items[i].sellIn--;
                 if (items[i].sellIn < 0) {
                     if (items[i].quality > 0) {
-                        items[i].quality = items[i].quality - 1;
+                        items[i].quality--;
                     }
                 }
             }
